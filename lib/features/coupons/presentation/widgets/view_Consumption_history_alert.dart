e9e6534ca26135e3bbab4e7fc0ed2c9c77f62c3f
Asmaa Elhendy <asmaaelhendy7055@gmail.com>
2025-08-12 16:55:22 +0300

    add add first coupon alert detail update

--- lib/features/coupons/presentation/widgets/view_Consumption_history_alert.dart
+++ lib/features/coupons/presentation/widgets/view_Consumption_history_alert.dart
@@ -24,118 +24,116 @@ class _ViewConsumptionHistoryAlertState
       insetPadding: EdgeInsets.all(16), // controls distance from screen edges
       child: SizedBox(
         width: MediaQuery.of(context).size.width * 0.94, // 90% screen width
-        height: MediaQuery.of(context).size.height * 0.48, // adjust height
+        height: MediaQuery.of(context).size.height * 0.5, // adjust height
         child: Padding(
           padding: EdgeInsets.symmetric(
             vertical: screenHeight * .02,
             horizontal: screenWidth * .05,
           ),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  customCouponPrimaryTitle(
+                    'Coupon Details',
+                    screenWidth,
+                    screenHeight,
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(
+                      Icons.close,
+                      size: screenWidth * .05,
+                      color: AppColors.greyDarktextIntExtFieldAndIconsHome,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: screenHeight * .01),
+              Container(
+                width: screenWidth,
+                // height: screenHeight*.1,
+                margin: EdgeInsets.only(bottom: screenHeight * .015),
+                padding: EdgeInsets.symmetric(
+                  vertical: screenHeight * .02,
+                  horizontal: screenWidth * .03,
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color(0xffECEBEA), // 👈 Border color
+                    width: 1, // 👈 Optional: Border thickness
+                  ),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    customCouponPrimaryTitle(
-                      'Coupon Details',
+                    Text(
+                      '1 Coupon Marked Used',
+                      style: TextStyle(
+                        color: AppColors.primary,
+                        fontWeight: FontWeight.w700,
+                        fontSize: screenWidth * .036,
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * .01),
+                    Text(
+                      'Marked On March 5, 2025',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: screenWidth * .036,
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * .02),
+                    Text(
+                      'Marked By',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: screenWidth * .036,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: screenHeight * .01,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Company 1',
+                            style: TextStyle(
+                              fontSize: screenWidth * .036,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          CouponStatus(screenHeight, screenWidth, 'Vendor'),
+                        ],
+                      ),
+                    ),
+                    BuildOutlinedIconButton(
                       screenWidth,
                       screenHeight,
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      icon: Icon(
-                        Icons.close,
-                        size: screenWidth * .05,
-                        color: AppColors.greyDarktextIntExtFieldAndIconsHome,
-                      ),
+                      'Show Delivery Photos',
+                      () {},
                     ),
                   ],
                 ),
-                SizedBox(height: screenHeight * .01),
-                Container(
-                  width: screenWidth,
-                  // height: screenHeight*.1,
-                  margin: EdgeInsets.only(bottom: screenHeight * .015),
-                  padding: EdgeInsets.symmetric(
-                    vertical: screenHeight * .02,
-                    horizontal: screenWidth * .03,
-                  ),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Color(0xffECEBEA), // 👈 Border color
-                      width: 1, // 👈 Optional: Border thickness
-                    ),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        '1 Coupon Marked Used',
-                        style: TextStyle(
-                          color: AppColors.primary,
-                          fontWeight: FontWeight.w700,
-                          fontSize: screenWidth * .036,
-                        ),
-                      ),
-                      SizedBox(height: screenHeight * .01),
-                      Text(
-                        'Marked On March 5, 2025',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontSize: screenWidth * .036,
-                        ),
-                      ),
-                      SizedBox(height: screenHeight * .02),
-                      Text(
-                        'Marked By',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: screenWidth * .036,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                          vertical: screenHeight * .01,
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text(
-                              'Company 1',
-                              style: TextStyle(
-                                fontSize: screenWidth * .036,
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                            CouponStatus(screenHeight, screenWidth, 'Vendor'),
-                          ],
-                        ),
-                      ),
-                      BuildOutlinedIconButton(
-                        screenWidth,
-                        screenHeight,
-                        'Show Delivery Photos',
-                        () {},
-                      ),
-                    ],
-                  ),
-                ),
+              ),
 
-                CancelOrderWidget(
-                  context,
-                  screenWidth,
-                  screenHeight,
-                  'Done',
-                  'Dispute',
-                ),
-              ],
-            ),
+              CancelOrderWidget(
+                context,
+                screenWidth,
+                screenHeight,
+                'Done',
+                'Dispute',
+              ),
+            ],
           ),
         ),
       ),
