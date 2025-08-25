ab3d09d0417858655bfe85a6568de4482e23dfcb
Asmaa Elhendy <asmaaelhendy7055@gmail.com>
2025-08-25 13:49:31 +0300

    add badge for gallon

--- lib/features/home/presentation/widgets/build_ForegroundAppBarHome.dart
+++ lib/features/home/presentation/widgets/build_ForegroundAppBarHome.dart
@@ -6,19 +6,28 @@ import 'package:iconify_flutter/icons/material_symbols.dart';
 import 'package:newwwwwwww/features/notification/presentation/pages/notification_screen.dart';
 import '../../../../core/theme/colors.dart';
 import '../../../cart/presentation/screens/cart_screen.dart';
+import 'package:badges/badges.dart' as badges;
 
 class BuildForegroundappbarhome extends StatefulWidget {
- double screenHeight;
- double screenWidth;
- String title;
- bool is_returned;
- String disabledCart;
- String disabledNotification;
- BuildForegroundappbarhome({required this.screenHeight,required this.screenWidth,required this.title,
-   required this.is_returned,this.disabledCart='',this.disabledNotification=''});
+  double screenHeight;
+  double screenWidth;
+  String title;
+  bool is_returned;
+  String disabledCart;
+  String disabledNotification;
+
+  BuildForegroundappbarhome({
+    required this.screenHeight,
+    required this.screenWidth,
+    required this.title,
+    required this.is_returned,
+    this.disabledCart = '',
+    this.disabledNotification = '',
+  });
 
   @override
-  State<BuildForegroundappbarhome> createState() => _BuildForegroundappbarhomeState();
+  State<BuildForegroundappbarhome> createState() =>
+      _BuildForegroundappbarhomeState();
 }
 
 class _BuildForegroundappbarhomeState extends State<BuildForegroundappbarhome> {
@@ -26,64 +35,114 @@ class _BuildForegroundappbarhomeState extends State<BuildForegroundappbarhome> {
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
-    return   Positioned(
-      top: MediaQuery
-          .of(context)
-          .padding
-          .top + widget.screenHeight * .05,
+    return Positioned(
+      top: MediaQuery.of(context).padding.top + widget.screenHeight * .05,
       left: 16,
       right: 16,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-           Row(mainAxisAlignment: MainAxisAlignment.start,
-             children: [
-               widget.is_returned?  GestureDetector(
-                 onTap: (){
-                   Navigator.pop(context);
-                 },
-                 child: Padding(
-                   padding:  EdgeInsets.only(right: screenWidth*.02,left: screenWidth*.02),
-                   child: Iconify(
-                     MaterialSymbols.arrow_back_ios,
-                     size: screenWidth*.05,
-                     color: AppColors.whiteColor,
-                   ),
-                 ),
-               ):SizedBox(),
-               Text(widget.title,
-                  style: TextStyle(
-                      color: AppColors.whiteColor,
-                      fontWeight: FontWeight.w400,
-                      fontSize: widget.screenWidth*.044)),
-             ],
-           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              widget.is_returned
+                  ? GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                          right: screenWidth * .02,
+                          left: screenWidth * .02,
+                        ),
+                        child: Iconify(
+                          MaterialSymbols.arrow_back_ios,
+                          size: screenWidth * .05,
+                          color: AppColors.whiteColor,
+                        ),
+                      ),
+                    )
+                  : SizedBox(),
+              Text(
+                widget.title,
+                style: TextStyle(
+                  color: AppColors.whiteColor,
+                  fontWeight: FontWeight.w400,
+                  fontSize: widget.screenWidth * .044,
+                ),
+              ),
+            ],
+          ),
           SizedBox(
             width: widget.screenWidth * .38,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Iconify(GameIcons.water_gallon,
-                    size: widget.screenWidth * .05, color: AppColors.whiteColor),
-                InkWell(
-                  onTap: (){
-                    widget.disabledNotification=='notifications'?null  : Navigator.push(context, MaterialPageRoute(builder: (context)=>NotificationScreen()));
-
-                  },
-                  child: Icon(Icons.notifications,
-                      color: AppColors.whiteColor, size: widget.screenWidth * .05),
+                badges.Badge(
+                  badgeContent: Text(
+                    '25',
+                    style: TextStyle(
+                      color: AppColors.whiteColor,
+                      fontSize: screenWidth * .028,
+                    ),
+                  ),
+                  badgeStyle: badges.BadgeStyle(
+                    badgeColor: AppColors.redColor,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: screenWidth * .006,
+                      vertical: screenHeight * .005,
+                    ),
+                    // 👈 ده اللي يتحكم في حجم البادج
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child:  Iconify(
+                    GameIcons.water_gallon,
+                    size: widget.screenWidth * .05,
+                    color: AppColors.whiteColor,
+                  ),
                 ),
                 InkWell(
-                  onTap: (){
-                 widget.disabledCart=='cart'?null  : Navigator.push(context, MaterialPageRoute(builder: (context)=>CartScreen()));
+                  onTap: () {
+                    widget.disabledNotification == 'notifications'
+                        ? null
+                        : Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => NotificationScreen(),
+                            ),
+                          );
                   },
-                  child: Icon(Icons.shopping_cart_outlined,
-                      color: AppColors.whiteColor, size: widget.screenWidth * .05),
+                  child: Icon(
+                    Icons.notifications,
+                    color: AppColors.whiteColor,
+                    size: widget.screenWidth * .05,
+                  ),
                 ),
-                SvgPicture.asset("assets/images/home/Language.svg",
-                    width: widget.screenWidth * .05),
-                SvgPicture.asset("assets/images/home/headphone.svg",
-                    width: widget.screenWidth * .05),
+                InkWell(
+                  onTap: () {
+                    widget.disabledCart == 'cart'
+                        ? null
+                        : Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => CartScreen(),
+                            ),
+                          );
+                  },
+                  child: Icon(
+                    Icons.shopping_cart_outlined,
+                    color: AppColors.whiteColor,
+                    size: widget.screenWidth * .05,
+                  ),
+                ),
+                SvgPicture.asset(
+                  "assets/images/home/Language.svg",
+                  width: widget.screenWidth * .05,
+                ),
+                SvgPicture.asset(
+                  "assets/images/home/headphone.svg",
+                  width: widget.screenWidth * .05,
+                ),
               ],
             ),
           ),
