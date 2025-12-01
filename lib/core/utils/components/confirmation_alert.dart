0fe554195675eb3a65a6d6d0b5a98eca00bc4deb
Asmaa Elhendy <asmaaelhendy7055@gmail.com>
2025-12-01 19:49:14 +0200

    add button in coupon screen

---
+++ lib/core/utils/components/confirmation_alert.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:newwwwwwww/features/orders/presentation/widgets/cancel_order_buttons.dart';
+import '../../../../core/theme/colors.dart';
+import '../../../features/coupons/presentation/widgets/custom_text.dart';
+
+class ConfirmationAlert extends StatefulWidget {
+  String centerTitle;
+String leftTtile;
+void Function() leftOnTap;
+ConfirmationAlert({required this.centerTitle,required this.leftTtile,required this.leftOnTap});
+
+  @override
+  State<ConfirmationAlert> createState() => _ConfirmationAlertState();
+}
+
+class _ConfirmationAlertState extends State<ConfirmationAlert> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Dialog(
+      // Use Dialog instead of AlertDialog
+      backgroundColor: AppColors.backgroundAlert,
+      insetPadding: EdgeInsets.all(16), // controls distance from screen edges
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.94, // 90% screen width
+        //    height: MediaQuery.of(context).size.height * 0.68, // adjust height
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: screenHeight * .04,
+            bottom: screenHeight * .04,
+            left: screenWidth * .05,
+            right: screenWidth * .05,
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                customCouponPrimaryTitle(
+                 widget.centerTitle,
+                  screenWidth,
+                  screenHeight,
+                ),
+
+
+                SizedBox(height: screenHeight * .02),
+                CancelOrderWidget(
+                  context,
+                  screenWidth,
+                  screenHeight,
+                  widget.leftTtile,
+                  'Cancel',
+                      widget.leftOnTap,
+                      () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
--- lib/features/coupons/presentation/widgets/add_coupon_widget.dart
+++ lib/features/coupons/presentation/widgets/add_coupon_widget.dart
@@ -1,25 +1,45 @@
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:newwwwwwww/features/home/presentation/pages/all_product_screen.dart';
 import '../../../../core/theme/colors.dart';
+import '../../../../core/utils/components/confirmation_alert.dart';
 
 Widget AddCoupon(BuildContext context,double screenWidth,double screenHeight){
-  return  InkWell(
-    onTap: (){
-      Navigator.push(context, MaterialPageRoute(builder: (context)=>AllProductScreen()));//where filter=coupons
-
+  return InkWell(
+    onTap: () {
+      showDialog(
+        context: context,
+        builder: (dialogContext) => ConfirmationAlert(
+          centerTitle: 'You Already Have 2 Coupons. Continue To Buy Another One?',
+          leftTtile: 'Continue Shopping',
+          leftOnTap: () {
+            // 👈 ده هيقفل الـ Dialog بس
+            Navigator.of(dialogContext).pop();
+            // go to products filter by coupons
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => AllProductScreen()),
+            );
+          },
+        ),
+      );
     },
     child: Container(
-      width: screenWidth*.1, // Set the desired width
-     // height: screenHeight*.1, // Set the desired height
+      width: screenWidth*.1,
       decoration: BoxDecoration(
-          shape: BoxShape.circle, // Makes the container circular
-          border: Border.all(
-            color: AppColors.primary, // Background color of the circle
-
-          )),
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: AppColors.primary,
+        ),
+      ),
       child: Center(
-        child:Icon(Icons.add,color: AppColors.primary,size: screenWidth*.065,),
+        child: Icon(
+          Icons.add,
+          color: AppColors.primary,
+          size: screenWidth*.065,
+        ),
       ),
     ),
   );
